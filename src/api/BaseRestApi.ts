fab4e3af774f5e851fbbf3e18946d49d2824570c
czh <120715979@qq.com>
2022-02-26 14:48:15 +0800

    axios配置

--- src/api/BaseRestApi.ts
+++ src/api/BaseRestApi.ts
@@ -1,8 +1,54 @@
-export default class BaseRestApi {
+import service from '@/utils/requests.ts'
+
+export default class BaseRestApi<T> {
     constructor(private uri: string) {
     }
 
-    public getById(id: number): any {
-        return null;
+    public getById(id: number): Promise<any> {
+        return service({
+            url: `/${this.uri}/${id}`,
+            method: 'get'
+        })
+    }
+
+    public getList(params: any): Promise<any> {
+        return service({
+            url: `/${this.uri}`,
+            method: 'get',
+            params
+        })
+    }
+
+    public create(data: T): Promise<any> {
+        return service({
+            url: `/${this.uri}`,
+            method: 'post',
+            data
+        })
+    }
+
+    public update(data: T): Promise<any> {
+        return service({
+            url: `/${this.uri}`,
+            method: 'put',
+            data
+        })
+    }
+
+    public delete(id: number): Promise<any> {
+        return service({
+            url: `/${this.uri}/${id}`,
+            method: 'delete'
+        })
+    }
+
+    public batchDelete(ids: Array<number>): Promise<any> {
+        return service({
+            url: `/${this.uri}/deletes`,
+            method: 'delete',
+            params: {
+                ids
+            }
+        })
     }
 }
